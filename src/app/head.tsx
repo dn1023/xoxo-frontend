c1fb993e216a2850dcf45240ebcbea6a8fb117f6
bruno <brunoprotz@outlook.com>
2024-08-07 22:32:32 +0200

    Update for clayton

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,28 +8,28 @@ export default function HeadIndex() {
       <meta property="og:title" content="XOXOBOT" />
       <meta property="og:description"
         content="Telegram Bot Manager" />
-      <meta property="og:url" content="https://xoxo.welovelaundry.uk/" />
+      <meta property="og:url" content="https://xoxobot.com/" />
       <meta property="og:image:type" content="image/png" />
-      <meta property="og:image" content="https://xoxo.welovelaundry.uk/_next/image?url=%2Fogimage.png&w=384&q=75" />
+      <meta property="og:image" content="https://xoxobot.com/_next/image?url=%2Fogimage.png&w=384&q=75" />
       <meta property="og:type" content="website" />
-      <meta property="og:logo" content="https://xoxo.welovelaundry.uk/_next/image?url=%2Ffavicon.png&w=384&q=75" />
-      <meta property="og:image:secure_url" content="https://xoxo.welovelaundry.uk/_next/image?url=%2Fogimage.png&w=384&q=75" />
+      <meta property="og:logo" content="https://xoxobot.com/_next/image?url=%2Ffavicon.png&w=384&q=75" />
+      <meta property="og:image:secure_url" content="https://xoxobot.com/_next/image?url=%2Fogimage.png&w=384&q=75" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:alt" content='XOXOBOT' />
       <meta property="og:image:width" content="600" />
       <meta property="og:image:height" content="300" />
-      <link rel="canonical" href="https://xoxo.welovelaundry.uk/" />
+      <link rel="canonical" href="https://xoxobot.com/" />
       <meta name="author" content="XOXOBOT" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="XOXOBOT" />
       <meta property="twitter:domain" content="xoxobot.com" />
-      <meta property="twitter:url" content="https://xoxo.welovelaundry.uk/" />
+      <meta property="twitter:url" content="https://xoxobot.com/" />
       <meta name="twitter:title" content="XOXOBOT" />
       <meta name="twitter:description" content="Telegram Bot Manager" />
-      <meta name="twitter:image" content="https://xoxo.welovelaundry.uk/_next/image?url=%2Fogimage.png&w=384&q=75" />
+      <meta name="twitter:image" content="https://xoxobot.com/_next/image?url=%2Fogimage.png&w=384&q=75" />
       <meta name="application-name" content="XOXOBOT" />
       <meta name="apple-mobile-web-app-title" content="" />
-      <link rel="icon" href="https://xoxo.welovelaundry.uk/_next/image?url=%2Ffavicon.ico&w=384&q=75" />
+      <link rel="icon" href="https://xoxobot.com/_next/image?url=%2Ffavicon.ico&w=384&q=75" />
       <title>XOXOBOT</title>
     </>
   )
